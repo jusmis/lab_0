794cecd7245d591806f045012281c18236932c97
jusmis <44554496+jusmis@users.noreply.github.com>
2019-03-07 18:45:52 +0100

    -195018

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        if (value == null)
+            return new ArrayList<>();
+        ArrayList<String> wynik = new ArrayList<>(value.length());
+        wynik.add(value);
+        for (int x = 1; x < value.length(); x++)
+            wynik.add(value.substring(x, value.length()).toUpperCase());
+        wynik.add("");
+        return wynik;
     }
 
 }
